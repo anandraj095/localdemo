66e3eae5e57801dbea979f42b7410188409a6f14
anandraj095 <anand24068@iiitd.ac.in>
2025-10-03 01:55:35 +0530

    Added hi

--- New.java
+++ New.java
@@ -4,5 +4,6 @@ public class New {
     public static void main(String[] args) {
         System.out.println("Anand Raj.");
         System.out.println("2024068");
+        System.out.println("Hi");
     }
 }
